ae0bdbc2d8188b49e69c45f5a6b5d76784f9876d
Abhishek Kumar Sumant <abhikr2210@gmail.com>
2024-06-04 14:48:58 +0530

    First Commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.tictactoe;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8;
+    TextView headerText;
+
+    int PLAYER_O = 0;
+    int PLAYER_X = 1;
+
+    int activePlayer = PLAYER_O;
+
+    int[] filledPos = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+    boolean isGameActive = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        headerText = findViewById(R.id.header_text);
+        headerText.setText("O's turn");
+
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+
+        btn0.setOnClickListener(this::onClick);
+        btn1.setOnClickListener(this::onClick);
+        btn2.setOnClickListener(this::onClick);
+        btn3.setOnClickListener(this::onClick);
+        btn4.setOnClickListener(this::onClick);
+        btn5.setOnClickListener(this::onClick);
+        btn6.setOnClickListener(this::onClick);
+        btn7.setOnClickListener(this::onClick);
+        btn8.setOnClickListener(this::onClick);
+
+    }
+
+
+    public void onClick(View v) {
+        // logic for button press
+
+        if(!isGameActive)
+            return;
+
+        Button clickedBtn = findViewById(v.getId());
+        int clickedTag = Integer.parseInt(v.getTag().toString());
+
+        if(filledPos[clickedTag] != -1){
+            return;
+        }
+
+        filledPos[clickedTag] = activePlayer;
+
+        if(activePlayer == PLAYER_O){
+            clickedBtn.setText("O");
+            clickedBtn.setBackgroundColor(getColor(R.color.blue));
+            activePlayer = PLAYER_X;
+            headerText.setText("X's turn");
+        }else{
+            clickedBtn.setText("X");
+            clickedBtn.setBackgroundColor(getColor(R.color.yellow));
+            activePlayer = PLAYER_O;
+            headerText.setText("O's turn");
+        }
+
+        checkForWin();
+
+    }
+
+    private void checkForWin(){
+        //we will check who is winner and show
+        int[][] winningPos = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+        for(int i =0 ;i<8;i++){
+            int val0  = winningPos[i][0];
+            int val1  = winningPos[i][1];
+            int val2  = winningPos[i][2];
+
+            if(filledPos[val0] == filledPos[val1] && filledPos[val1] == filledPos[val2]){
+                if(filledPos[val0] != -1){
+                    //winner declare
+
+                    isGameActive = false;
+
+                    if(filledPos[val0] == PLAYER_O)
+                        showDialog("O is winner");
+                    else
+                        showDialog("X is winner");
+                }
+            }
+        }
+
+
+    }
+
+    private void showDialog(String winnerText){
+        new AlertDialog.Builder(this)
+                .setTitle(winnerText)
+                .setPositiveButton("Restart game", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        restartGame();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private void restartGame(){
+        activePlayer = PLAYER_O;
+        headerText.setText("O's turn");
+        filledPos = new int[]{-1,-1,-1,-1,-1,-1,-1,-1,-1};
+        btn0.setText("");
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+
+        btn0.setBackground(getDrawable(android.R.color.darker_gray));
+        btn1.setBackground(getDrawable(android.R.color.darker_gray));
+        btn2.setBackground(getDrawable(android.R.color.darker_gray));
+        btn3.setBackground(getDrawable(android.R.color.darker_gray));
+        btn4.setBackground(getDrawable(android.R.color.darker_gray));
+        btn5.setBackground(getDrawable(android.R.color.darker_gray));
+        btn6.setBackground(getDrawable(android.R.color.darker_gray));
+        btn7.setBackground(getDrawable(android.R.color.darker_gray));
+        btn8.setBackground(getDrawable(android.R.color.darker_gray));
+        isGameActive = true;
+    }
+}
